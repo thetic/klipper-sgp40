9e8318f6b9412caa9c663861e42b38beed04d66d
Chad Condon <chad@condon.tech>
2024-11-15 00:46:30 -0800

    Cleanup install

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,9 +6,15 @@ set -e
 KLIPPER_PATH="${HOME}/klipper"
 KLIPPER_SERVICE_NAME=klipper
 
+SCRIPTS=(
+    "sgp40.py"
+    "voc_algorithm.py"
+)
+
 usage() {
     echo "Usage: $0 [-k <klipper path>] [-s <klipper service name>] [-c <configuration path>] [-u]" 1>&2
 }
+
 # Parse command line arguments
 while getopts "k:s:c:uh" arg; do
     case $arg in
@@ -28,60 +34,52 @@ SRCDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"/src/ && pwd)"
 
 # Verify Klipper has been installed
 check_klipper() {
-    if [ "$(sudo systemctl list-units --full -all -t service --no-legend | grep -F "$KLIPPER_SERVICE_NAME.service")" ]; then
-        echo "Klipper service found with name "$KLIPPER_SERVICE_NAME"."
+    if sudo systemctl list-units --full -all -t service --no-legend | grep -qF "$KLIPPER_SERVICE_NAME.service"; then
+        echo "Klipper service found with name $KLIPPER_SERVICE_NAME."
     else
-        echo "[ERROR] Klipper service with name "$KLIPPER_SERVICE_NAME" not found, please install Klipper first or specify name with -s."
-        exit -1
+        echo "[ERROR] Klipper service with name $KLIPPER_SERVICE_NAME not found, please install Klipper first or specify name with -s."
+        exit 1
     fi
 }
 
 check_folders() {
-    if [ ! -d "$KLIPPER_PATH/klippy/extras/" ]; then
-        echo "[ERROR] Klipper installation not found in directory \"$KLIPPER_PATH\". Exiting"
-        exit -1
+    if [ -d "$KLIPPER_PATH/klippy/extras/" ]; then
+        echo "Klipper installation found at $KLIPPER_PATH"
+    else
+        echo "[ERROR] Klipper installation not found in directory $KLIPPER_PATH. Exiting"
+        exit 1
     fi
-    echo "Klipper installation found at $KLIPPER_PATH"
 }
 
 # Link extension to Klipper
 link_extension() {
     echo -n "Linking extension to Klipper... "
-    ln -sf "${SRCDIR}/sgp40.py" "${KLIPPER_PATH}/klippy/extras/sgp40.py"
-    ln -sf "${SRCDIR}/voc_algorithm.py" "${KLIPPER_PATH}/klippy/extras/voc_algorithm.py"
-    echo "[OK]"
-}
-
-# Restart moonraker
-restart_moonraker() {
-    echo -n "Restarting Moonraker... "
-    sudo systemctl restart moonraker
-    echo "[OK]"
-}
-
-restart_klipper() {
-    echo -n "Restarting Klipper... "
-    sudo systemctl restart $KLIPPER_SERVICE_NAME
+    for script in "${SCRIPTS[@]}"; do
+        echo -n "${script}... "
+        ln -sf "${SRCDIR}/${script}" "${KLIPPER_PATH}/klippy/extras/${script}"
+    done
     echo "[OK]"
 }
 
 start_klipper() {
     echo -n "Starting Klipper... "
-    sudo systemctl start $KLIPPER_SERVICE_NAME
+    sudo systemctl start "$KLIPPER_SERVICE_NAME"
     echo "[OK]"
 }
 
 stop_klipper() {
     echo -n "Stopping Klipper... "
-    sudo systemctl stop $KLIPPER_SERVICE_NAME
+    sudo systemctl stop "$KLIPPER_SERVICE_NAME"
     echo "[OK]"
 }
 
 uninstall() {
     echo -n "Uninstalling... "
 
-    rm -f "${KLIPPER_PATH}/klippy/extras/sgp40.py"
-    rm -f "${KLIPPER_PATH}/klippy/extras/voc_algorithm.py"
+    for script in "${SCRIPTS[@]}"; do
+        echo -n "${script}... "
+        rm -f "${KLIPPER_PATH}/klippy/extras/${script}"
+    done
 
     echo "[OK]"
     echo "You can now remove the [update_manager klipper-sgp40] section in your moonraker.conf and delete this directory. Also remove all sgp40 configurations from your Klipper configuration."
@@ -91,7 +89,7 @@ uninstall() {
 verify_ready() {
     if [ "$EUID" -eq 0 ]; then
         echo "[ERROR] This script must not run as root. Exiting."
-        exit -1
+        exit 1
     fi
 }
 
@@ -100,9 +98,9 @@ verify_ready
 check_klipper
 check_folders
 stop_klipper
-if [ ! $UNINSTALL ]; then
-    link_extension
-else
+if [ "$UNINSTALL" -eq 1 ]; then
     uninstall
+else
+    link_extension
 fi
 start_klipper
